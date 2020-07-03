82db6f9703d04f26558fbd23b07346fb3ddde439
Anand Chowdhary <mail@anandchowdhary.com>
2020-07-03 13:22:10 +0530

    :sparkles: Add service

--- index.ts
+++ index.ts
@@ -1,4 +1,89 @@
 import { config } from "dotenv";
-import {} from "firebase-admin";
-
+import {
+  initializeApp,
+  credential,
+  ServiceAccount,
+  firestore,
+} from "firebase-admin";
+import axios from "axios";
 config();
+
+const FIREBASE_SERVICE_ACCOUNT_KEY: ServiceAccount = JSON.parse(
+  process.env.FIREBASE_SERVICE_ACCOUNT_KEY
+);
+const SLACK_WEBHOOK_KEY = process.env.SLACK_WEBHOOK_KEY;
+const FIREBASE_DATABASE_URL = process.env.FIREBASE_DATABASE_URL;
+
+initializeApp({
+  credential: credential.cert(FIREBASE_SERVICE_ACCOUNT_KEY),
+  databaseURL: FIREBASE_DATABASE_URL,
+});
+const startTime = new Date();
+startTime.setHours(-1);
+startTime.setMinutes(0);
+startTime.setSeconds(0);
+startTime.setMilliseconds(0);
+const collection = firestore()
+  .collection("subscribers")
+  .where("date", ">=", startTime);
+
+const getData = async () => {
+  const snapshot = await collection.get();
+  const data: any[] = [];
+  snapshot.forEach((record) => {
+    data.push(record.data());
+  });
+  return data;
+};
+
+/** Post a message to a Slack channel */
+export const postToSlack = async (data: any) => {
+  const payload = {
+    username: "Koj Bot",
+    icon_url:
+      "https://raw.githubusercontent.com/AnandChowdhary/rescuetime-slack/master/assets/bot.png",
+    blocks: [
+      {
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text: `*${data.name} just signed up!`,
+        },
+      },
+      {
+        type: "section",
+        fields: [
+          {
+            type: "mrkdwn",
+            text: `*Email* \n ${data.email}`,
+          },
+          {
+            type: "mrkdwn",
+            text: `*Phone* \n ${data.phone}`,
+          },
+          {
+            type: "mrkdwn",
+            text: `*Period* \n ${data.subscriptionPeriod} years`,
+          },
+          {
+            type: "mrkdwn",
+            text: `*Budget* \n CHF ${data.budget}`,
+          },
+        ],
+      },
+    ],
+  };
+  await axios.post(
+    `https://hooks.slack.com/services/${SLACK_WEBHOOK_KEY}`,
+    payload
+  );
+};
+
+const firebaseSlack = async () => {
+  const data = await getData();
+  for await (const item of data) {
+    postToSlack(item);
+  }
+};
+
+firebaseSlack();
