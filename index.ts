09f65857d176b86a616a2d49d8e0751f53f027a4
Anand Chowdhary <mail@anandchowdhary.com>
2020-08-28 16:07:40 +0530

    :recycle: Use Slack app

--- index.ts
+++ index.ts
@@ -26,47 +26,44 @@ const realEstate = firestore().collection("real-estate-managers");
 export const postToSlack = async (data: any, id: string) => {
   if (data.dev) return;
   console.log("Triggering webhook");
-  const payload = {
-    username: "Koj Bot",
-    icon_url:
-      "https://raw.githubusercontent.com/AnandChowdhary/rescuetime-slack/master/assets/bot.png",
-    blocks: [
-      {
-        type: "section",
-        text: {
-          type: "mrkdwn",
-          text: data.realEstate
-            ? `Real estate manager lead from *${data.name || "a user"}* (${
-                data.email
-              })`
-            : `*${data.name || "A user"}* (${
-                data.email
-              }) just signed up for a *${
-                data.numberOfRooms
-              }-room* apartment in *${data.locationName}* for a period of *${
-                data.period * 12
-              } months* with a budget of *${data.budget} CHF/month*.`,
-        },
+  const blocks = [
+    {
+      type: "section",
+      text: {
+        type: "mrkdwn",
+        text: data.realEstate
+          ? `Real estate manager lead from *${data.name || "a user"}* (${
+              data.email
+            })`
+          : `*${data.name || "A user"}* (${data.email}) just signed up for a *${
+              data.numberOfRooms
+            }-room* apartment in *${data.locationName}* for a period of *${
+              data.period * 12
+            } months* with a budget of *${data.budget} CHF/month*.`,
       },
-      data.realEstate
-        ? undefined
-        : {
-            type: "section",
-            text: {
-              type: "mrkdwn",
-              text: `Full lead details: https://koj.co/en-ch/admin/customers/${id}`,
-            },
+    },
+    data.realEstate
+      ? undefined
+      : {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: `Full lead details: https://koj.co/en-ch/admin/customers/${id}`,
           },
-    ],
-  };
-  try {
-    await axios.post(
-      `https://hooks.slack.com/services/${SLACK_WEBHOOK_KEY}`,
-      payload
-    );
-  } catch (error) {
-    console.log(JSON.stringify(payload, null, 2));
-  }
+        },
+  ];
+  await axios.post(
+    "https://slack.com/api/chat.postMessage",
+    {
+      channel: "C016A9X32KG",
+      blocks,
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.SLACK_BOT_ACCESS_TOKEN}`,
+      },
+    }
+  );
 };
 
 const sent: string[] = [];
